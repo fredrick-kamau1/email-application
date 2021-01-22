8b5103042c2885d72cfb1763be8950039b58d7ce
fredrick-kamau1 <70659811+fredrick-kamau1@users.noreply.github.com>
2021-01-22 16:39:19 -0600

    Edits

--- EmailApplication/src/EmailApplication/EmailMain.java
+++ EmailApplication/src/EmailApplication/EmailMain.java
@@ -1,5 +1,5 @@
 package EmailApplication;
-import java.util.Scanner;
+
 /**
  * @author Fredrick Kamau class EmailMain which takes in user entered 
  * credentials and creates email accounts for new hires.  
@@ -7,32 +7,10 @@ import java.util.Scanner;
 public class EmailMain
 {
   public static void main(String[] arg)
-  {
-    Scanner input = userInput();    
-    
+  {       
     //Generate new employee email 
-    Email newEmployee = new Email(firstName(input), secondName(input));
+    Email newEmployee = new Email();
     System.out.println(newEmployee.showInfo());
-    input.close(); 
   }
-  
-  //Create a new scanner object
-  public static Scanner userInput()
-  {    
-    return new Scanner(System.in);
-  }
-  
-  //Get first name
-  public static String firstName(Scanner input)
-  {
-    System.out.print("Enter First Name: ");
-    return input.nextLine();
-  }
-  
-  //Get second name
-  public static String secondName(Scanner input)
-  {
-    System.out.print("Enter Last Name: ");
-    return input.nextLine();
-  }
+ 
 }//end class
